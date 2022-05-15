92c271efb6971e6bd5cbe5272842596399b18949
Mathius <mathiuskormasela12@gmail.com>
2022-05-15 23:07:30 +0700

    Pertemuan 1

---
+++ pertemuan1/index.py
@@ -0,0 +1,11 @@
+# =============== Belajar Mencetak Sesuatu Ke Layar ===============
+# print merupakan sebauh function yg berfungsi untuk
+# mencetak sesuatu kelayar, bisa berupa nilai dari
+# sebuah variable, string, number, boolean dan lain-lain.
+
+# Untuk mencetak sesuatu ke layar
+print('Hello World')
+print("Hello Python")
+print(18)
+print(True)
+print(False)
