e2ad84484bfcd2913cab5271c30e45faa84dbf76
Mathius <mathiuskormasela12@gmail.com>
2022-05-15 23:28:47 +0700

    pertemuan2

---
+++ pertemuan2/index.py
@@ -0,0 +1,33 @@
+# =============== Belajar Membuat Variable & Mengenal Tipe Data ===============
+# A. Variable
+#    Variable merupakan cara kita untuk memberikan
+#    sebuah nama terhadap suatu nilai.
+#    rumus :
+#    namaVariable = nilainya
+#
+# B. Data Type (Tipe Data)
+#    Tipe Data merupkan jenis dari nilai suatu variable. 
+#    Di dalam Python kita mengenal tipe data string, numbers (int, float, complex),  
+#    boolean, list, tupple dan dictionary.
+
+# Membuat Variable nama dengan bertipe data string
+# tipe data string dapat di tulis dengan single atau double quote
+nama = 'Mathius'
+marga = "Kormasela2"
+print(nama, marga)
+
+# Membuat variable usia dengan tipe data int
+usia = 20
+print(usia)
+
+# Membuat variable tinggi badan dengan tipe data float
+tinggiBadan = 166.5
+print(tinggiBadan)
+
+# Membuat variable menikah dengan tipa data boolean
+menikah = False
+blmMenikah = True
+print(menikah)
+
+# Mengecek tipe data dari sebuah variable
+print(type(menikah))
