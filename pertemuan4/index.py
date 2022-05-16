d79bd08aee2c93475046dd46a19bac102cd4f5a8
Mathius <mathiuskormasela12@gmail.com>
2022-05-16 08:38:30 +0700

    Pertemuan4

---
+++ pertemuan4/index.py
@@ -0,0 +1,32 @@
+# =============== Belajar Operator Aritmatika & Method Number ===============
+import math
+
+# Operator aritmatika
+print(2 * 4)
+print(2 + 4)
+print(2 - 4)
+print(4 / 2)
+print(8 % 2)
+
+# Method number
+# mencari angka terkecil
+print(min(4, 5,1, 7 ,2))
+# Mencari angka terbesar
+print(max(6, 9, 1, 6, 10, 7))
+
+# membulatkan angka ke angka terdekat
+# 1 - 5 di bukatkan ke bawah
+# 6 - 9 di bulatkan ke atas
+print(round(6.6))
+
+# menampilkna angka pi
+print(math.pi)
+
+# membulatkan ke bawah
+print(math.floor(6.9))
+
+# membulatkan ke atas
+print(math.ceil(6.1))
+
+# membuat perpangkatan (2 pangkat 3)
+print(math.pow(2, 3))
