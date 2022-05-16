a7e7890d5927dd95c2dbe917b4a438aaace0eaab
Mathius <mathiuskormasela12@gmail.com>
2022-05-16 08:23:43 +0700

    pertemuan3

---
+++ pertemuan3/index.py
@@ -0,0 +1,28 @@
+# ============ Belajar Concatination, Escaping & Method Pada String ============
+# A. Concatination
+#    Concatiation merupakan cara kita untuk 
+#    menggabungkan dua string atau lebih.
+#    Dalam Python kita menggunakan tanda '+'
+#    untuk melakukan concatination.
+
+# Concatination
+nama = 'Mathius'
+marga = 'Kormasela'
+print(nama + ' ' + marga)
+
+# Escaping
+print("Mathius \"Kormasela\"")
+
+# Membuat baris baru
+print("Mathius \n Kormasela")
+
+# membuat tab
+print("Mathius \t KOrmaselaa")
+
+# Mengubah huruf depan suatu string menjadi kapital
+bhs = 'python'
+print(bhs.capitalize())
+
+# Mengubah sting
+nama2 = 'Mathius'
+print(nama2.replace('th', 't'))
