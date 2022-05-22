05d1258bc978b58fe2ac13d0516e79201d861a17
Mathius <mathiuskormasela12@gmail.com>
2022-05-22 17:32:23 +0700

    Belajar membuat commentar dan meminta inputan dari user

---
+++ pertemuan6/index.py
@@ -0,0 +1,15 @@
+"""
+	A. Commentar
+	   Commentar berfungsi untuk mematikan
+		 sebuah baris kode. Selain itu, Commentar
+		 berfungsi juga untuk menuliskan siapa yang
+		 menulis kodenya, siapa yang mengedit dan juga
+		 untuk menjelasakna suatu baris kode. Oleh
+		 sebab itu Commentar tidak akan di eksekusi
+		 oleh Python.
+"""
+
+# Single Line Commentar
+"""
+	Multi Line Commentar
+"""
---
+++ pertemuan7/index.py
@@ -0,0 +1,7 @@
+# Belajar Meminta inputan dari user
+
+# Meminta inputan user dan memasukannya ke dalam sebuah variable
+nama = input('Masukan nama anda : ')
+usia = input('Masukan usia anda : ')
+
+print("You are Mathius and " + usia + " years old")
